3576abc06a021d47b00a46258584b7f1a658c8a2
TodTheSushiMonster <nconzetti@gmail.com>
2025-01-05 18:01:59 -0800

    Knockback direction fix
    Now just goes in the direction it's going, and adds a little bit based on where it is relatively

--- Assets/Scripts/Game/Combat/Bear/BearDamageDealer.cs
+++ Assets/Scripts/Game/Combat/Bear/BearDamageDealer.cs
@@ -6,14 +6,18 @@ namespace Game.Combat.Bear {
     public class BearDamageDealer : MonoBehaviour {
         [SerializeField] private int damage = 50;
         [SerializeField] private float knockbackForce = 10;
+        [Range(0f,1f)]
+        [SerializeField] private float directionWeight = 0.6f;
         
         private bool _canDamage = true;
         
         private void OnTriggerEnter2D(Collider2D other) {
             if (!_canDamage) return;
             if (!other.CompareTag(Constants.Tags.Enemy)) return;
+
+            Vector2 dif= (other.transform.position - transform.position).normalized * directionWeight;
             
-            Vector2 knockbackDirection = (other.transform.position - transform.position).normalized;
+            Vector2 knockbackDirection = (Vector2)transform.right + dif;
             
             other.GetComponent<EnemyBase>().TakeDamage(50, knockbackDirection, knockbackForce);
         }
