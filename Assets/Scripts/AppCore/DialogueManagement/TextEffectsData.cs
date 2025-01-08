86653c7efadbb2432cff23da316591973fdb9b38
TodTheSushiMonster <nconzetti@gmail.com>
2025-01-08 08:41:28 -0800

    Added dialogue animation setup
    Refactored animation setup, added animating characters when they come in. Fixed goofy text flicking.

--- Assets/Scripts/AppCore/DialogueManagement/TextEffectsData.cs
+++ Assets/Scripts/AppCore/DialogueManagement/TextEffectsData.cs
@@ -4,5 +4,7 @@ namespace AppCore.DialogueManagement {
     [CreateAssetMenu(fileName = "TextEffectsData", menuName = "Dialogue/TextEffectsData")]
     public class TextEffectsData : ScriptableObject {
         [SerializeField] public WobbleData[] wobbleData;
+        [SerializeField] public float appearDuration = 0.1f;
+        [SerializeField] public float appearHeight = 2f;
     }
 }
