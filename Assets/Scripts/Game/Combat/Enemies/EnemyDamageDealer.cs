a6399ba3dca00f3b92f5fc743601b1a109aa6e7e
TodTheSushiMonster <nconzetti@gmail.com>
2025-01-22 23:09:52 -0800

    VFX Graph

--- Assets/Scripts/Game/Combat/Enemies/EnemyDamageDealer.cs
+++ Assets/Scripts/Game/Combat/Enemies/EnemyDamageDealer.cs
@@ -6,7 +6,6 @@ namespace Game.Combat.Enemies {
         [SerializeField] private int damage = 50;
         
         private bool _canDamage = true;
-        
         private void OnTriggerEnter2D(Collider2D other) {
             if (!_canDamage) return;
             if (!other.CompareTag(Constants.Tags.Child)) return;
